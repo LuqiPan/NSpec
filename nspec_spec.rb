e0320aa071e87f16883ccf5c393564504594b951
Luqi Pan <luqi.pan@gmail.com>
2014-07-22 21:20:03 -0400

    Worked with variables, at least I thought

--- nspec_spec.rb
+++ nspec_spec.rb
@@ -24,4 +24,14 @@ describe NSpec do
     result = parser.parse('expect (it) not not to be equal 5')
     expect(result).to be_nil
   end
+
+  it 'also works with variable with underscore' do
+    result = parser.parse('expect (var_) to be equal something')
+    expect(result).to be_a(Treetop::Runtime::SyntaxNode)
+  end
+
+  it 'also works with variable with number' do
+    result = parser.parse('expect (var1) to be equal something')
+    expect(result).to be_a(Treetop::Runtime::SyntaxNode)
+  end
 end
