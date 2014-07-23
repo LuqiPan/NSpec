2b08887e4ba4d8a6a716f9a9e1021c514b4154ab
Luqi Pan <luqi.pan@gmail.com>
2014-07-22 21:09:25 -0400

    Made it more flexible

--- nspec_spec.rb
+++ nspec_spec.rb
@@ -5,13 +5,23 @@ Treetop.load 'NSpec'
 describe NSpec do
   let(:parser) { NSpecParser.new }
 
-  it 'parses the sentence without some white spaces' do
-    result = parser.parse('expect(it) to be equal 5')
-    expect(result).to be_a(Treetop::Runtime::SyntaxNode)
-  end
-
   it 'parse the sentence with white spaces' do
     result = parser.parse('expect (it) to be equal 5')
     expect(result).to be_a(Treetop::Runtime::SyntaxNode)
+  end
+
+  it 'parse the sentence with not' do
+    result = parser.parse('expect (it) not to be equal 5')
+    expect(result).to be_a(Treetop::Runtime::SyntaxNode)
+  end
+
+  it 'be can be optional' do
+    result = parser.parse('expect (it) not to equal 5')
+    expect(result).to be_a(Treetop::Runtime::SyntaxNode)
+  end
+
+  it "doesn't parse 2 'not's" do
+    result = parser.parse('expect (it) not not to be equal 5')
+    expect(result).to be_nil
   end
 end
