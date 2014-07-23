829dde0ace0d6a510afbbc1c6fd1e25f091114d7
Luqi Pan <luqi.pan@gmail.com>
2014-07-23 19:57:25 -0400

    Got rid of parens in my syntax

--- spec/nspec_spec.rb
+++ spec/nspec_spec.rb
@@ -22,16 +22,16 @@ describe NSpec do
 
   describe 'parse the sentence' do
     with_pairs = [
-      ['with white spaces', 'expect (it) to be equal 5'],
-      ['with not', 'expect (it) not to be equal 5'],
-      ['with variable containing underscore', 'expect (var_) to be equal something'],
-      ['with variable containing number', 'expect (var1) to be equal something'],
-      ['with variable containing both underscore and number', 'expect (var_1) to be equal s']
+      ['with white spaces', 'expect it to be equal 5'],
+      ['with not', 'expect it not to be equal 5'],
+      ['with variable containing underscore', 'expect var_ to be equal something'],
+      ['with variable containing number', 'expect var1 to be equal something'],
+      ['with variable containing both underscore and number', 'expect var_1 to be equal s'],
     ]
     without_pairs = [
-      ['without be', 'expect (it) not to equal 5'],
-      ['without space after expect', 'expect(it) to eq 5'],
-      ['without space after actual', 'expect(it)to eq 5']
+      ['without be', 'expect it not to equal 5'],
+      ['without space after expect', 'expect it to eq 5'],
+      ['without space after actual', 'expect it to eq 5']
     ]
     (with_pairs + without_pairs).each do |pair|
       context pair[0] do
@@ -43,7 +43,7 @@ describe NSpec do
 
   describe "doesn't parse the sentence" do
     pairs = [
-      ['with 2 nots', 'expect (it) not not to be equal 5']
+      ['with 2 nots', 'expect it not not to be equal 5']
     ]
     pairs.each do |pair|
       context pair[0] do
