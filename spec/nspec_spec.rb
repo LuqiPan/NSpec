c5eb03f547fa0eb5dc1982b383ce7b1aff06c745
Luqi Pan <luqi.pan@gmail.com>
2014-07-23 19:15:25 -0400

    Added optional spaces

--- spec/nspec_spec.rb
+++ spec/nspec_spec.rb
@@ -21,15 +21,18 @@ describe NSpec do
   let(:parser) { NSpecParser.new }
 
   describe 'parse the sentence' do
-    pairs = [
+    with_pairs = [
       ['with white spaces', 'expect (it) to be equal 5'],
       ['with not', 'expect (it) not to be equal 5'],
-      ['without be', 'expect (it) not to equal 5'],
       ['with variable containing underscore', 'expect (var_) to be equal something'],
       ['with variable containing number', 'expect (var1) to be equal something'],
       ['with variable containing both underscore and number', 'expect (var_1) to be equal s']
     ]
-    pairs.each do |pair|
+    without_pairs = [
+      ['without be', 'expect (it) not to equal 5'],
+      ['without space after expect', 'expect(it) to eq 5']
+    ]
+    (with_pairs + without_pairs).each do |pair|
       context pair[0] do
         subject { pair[1] }
         it_behaves_like 'an accepted sentence'
