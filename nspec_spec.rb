920b44a80637049c868cdb0acff8b23af2259f44
Luqi Pan <luqi.pan@gmail.com>
2014-07-22 22:53:34 -0400

    Tried to extract the common pattern in specs

--- nspec_spec.rb
+++ nspec_spec.rb
@@ -2,7 +2,7 @@ require 'treetop'
 require 'pry'
 Treetop.load 'NSpec'
 
-shared_examples "an AST" do
+shared_examples "an accepted sentence" do
   it 'is a SyntaxNode' do
     result = parser.parse(subject)
     expect(result).to be_a(Treetop::Runtime::SyntaxNode)
@@ -13,35 +13,37 @@ describe NSpec do
   let(:parser) { NSpecParser.new }
   let(:sentence) { 'not gonna happen' }
 
-  describe 'parse the sentence with white spaces' do
-    subject { 'expect (it) to be equal 5' }
-    it_behaves_like 'an AST'
-    #result = parser.parse('expect (it) to be equal 5')
-    #expect(result).to be_a(Treetop::Runtime::SyntaxNode)
-  end
+  describe 'parse the sentence' do
+    context 'with white spaces' do
+      subject { 'expect (it) to be equal 5' }
+      it_behaves_like 'an accepted sentence'
+      #result = parser.parse('expect (it) to be equal 5')
+      #expect(result).to be_a(Treetop::Runtime::SyntaxNode)
+    end
 
-  it 'parse the sentence with not' do
-    result = parser.parse('expect (it) not to be equal 5')
-    expect(result).to be_a(Treetop::Runtime::SyntaxNode)
-  end
+    it 'parse the sentence with not' do
+      result = parser.parse('expect (it) not to be equal 5')
+      expect(result).to be_a(Treetop::Runtime::SyntaxNode)
+    end
 
-  it 'be can be optional' do
-    result = parser.parse('expect (it) not to equal 5')
-    expect(result).to be_a(Treetop::Runtime::SyntaxNode)
-  end
+    it 'be can be optional' do
+      result = parser.parse('expect (it) not to equal 5')
+      expect(result).to be_a(Treetop::Runtime::SyntaxNode)
+    end
 
-  it "doesn't parse 2 'not's" do
-    result = parser.parse('expect (it) not not to be equal 5')
-    expect(result).to be_nil
-  end
+    it "doesn't parse 2 'not's" do
+      result = parser.parse('expect (it) not not to be equal 5')
+      expect(result).to be_nil
+    end
 
-  it 'also works with variable with underscore' do
-    result = parser.parse('expect (var_) to be equal something')
-    expect(result).to be_a(Treetop::Runtime::SyntaxNode)
-  end
+    it 'also works with variable with underscore' do
+      result = parser.parse('expect (var_) to be equal something')
+      expect(result).to be_a(Treetop::Runtime::SyntaxNode)
+    end
 
-  it 'also works with variable with number' do
-    result = parser.parse('expect (var1) to be equal something')
-    expect(result).to be_a(Treetop::Runtime::SyntaxNode)
+    it 'also works with variable with number' do
+      result = parser.parse('expect (var1) to be equal something')
+      expect(result).to be_a(Treetop::Runtime::SyntaxNode)
+    end
   end
 end
